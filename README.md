83a582927469d08e8fdf75e57f4dd47a74d929ef
Lousine <l.h.avetisyan@gmail.com>
2023-12-07 16:15:19 +0400

    Add a new solution

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Solutions to the Codewars problems.
 | Kata  | kyu  | URL |
 | :--------------------------------- |:---------------:| -----:|
 | Extract the domain name from a URL | 5 | [Link](extract_the_domain_name_from_a_url) |
-| NA      | 5        |   NA |
+| Multiples of 3 or 5      | 6  |   [Link](multiples_of_3_or_5) |
 | NA | 5        |    NA |
--- extract_the_domain_name_from_a_url/solution_extract_the_domain_name_from_a_url.py
+++ extract_the_domain_name_from_a_url/solution_extract_the_domain_name_from_a_url.py
@@ -1,2 +1,22 @@
 def domain_name(url):
-    
+    """
+    Extracts the domain name from a URL without using additional imports.
+
+    :param url: A string representing the URL.
+    :type url: str
+    :return: The extracted domain name as a string.
+    :rtype: str or None
+    """
+    url = url.replace("http://","").replace("https://","")
+    url = url.replace("www.","")
+
+    parts = url.split(".")
+
+    if len(parts) >1:
+        return parts[0]
+    else:
+        return None
+
+def domain_name_short(url):
+    """ Short solution to the same kata. """
+    return url.split("//")[-1].split("www.")[-1].split(".")[0]
